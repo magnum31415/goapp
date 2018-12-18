6ce9b27facf63adf29c3bd0c2070c37f994c7f1d
Ricard Larios Cuerba <rlarios@externos.itnow.es>
2018-12-18 12:47:54 +0100

    Initial version

---
+++ prova.go
@@ -0,0 +1,12 @@
+//https://medium.com/google-cloud/building-a-go-web-app-from-scratch-to-deploying-on-google-cloud-part-1-building-a-simple-go-aee452a2e654
+//http://tf7seasd01.lacaixa.es/aandino/consultant-front
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello world")
+}
